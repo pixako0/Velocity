a3c3412e8b8bca103ee1a9ac4a5a8c057709dbe9
Fulgur <fulgurgithub@gmail.com>
2025-03-19 20:13:26 +0100

    enemy class

---
+++ enemy.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Ship.h";
+
+namespace Tmpl8 {
+	class Planet;
+	class Enemy
+	{
+	public:
+		float x = 0;
+		float y = 0;
+		float acceleration = 0.6;
+
+		float spawnX = -6080;
+		float spawnY = -2000;
+
+		int maxVelocity = 100;
+
+		float velocityX = 0;
+		float velocityY = 0;
+
+		bool moveRight = false;
+		bool moveLeft = false;
+		bool moveUp = false;
+		bool moveDown = false;
+
+		void move();
+		void die();
+		
+		void intialize();
+
+		Ship ship;
+	private:
+	};
+}
+
